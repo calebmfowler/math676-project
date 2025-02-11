e07cca4e043565d4597c630b2517b0d77c5b1871
Caleb Fowler <caleb.fowler.atx@gmail.com>
2025-02-11 13:52:56 -0600

    moved class exercises elsewhere

--- class-exercises/1_28.2.cpp
+++
@@ -1,8 +0,0 @@
-#include <iostream>
-
-int main() {
-    const auto meaningAdder = [](unsigned int &num) { num += 42; };
-
-    unsigned int num = 0;
-    std::cout << "initial: " << num << std::endl;
-}
